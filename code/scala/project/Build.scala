89e048ca49f78e0499d20dacdfd457366ac224b5
Tomo Ferega <tomo@element.hr>
2011-11-06 16:46:58 +0100

    Version bump

--- code/scala/project/Build.scala
+++ code/scala/project/Build.scala
@@ -9,7 +9,7 @@ object ProjectInfo {
   val Organization = "hr.element.etb"
   val Name = "FormatNazy"
   val PublishName = Name.toLowerCase
-  val Version = "0.0.4"
+  val Version = "0.0.5"
   val ScalaVersion = "2.9.1"
 }
 
